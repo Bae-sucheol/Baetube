1ccf0efff6cd5d1b266d0b3f84b5f63d2bc43ee9
Bae-sucheol <81005707+Bae-sucheol@users.noreply.github.com>
2022-11-03 20:29:18 +0900

    2022/11/03
    1. 11/02 부터 안드로이드 사전 설정 및 기본 설정.
    2. 2022/11/04 즉 내일부터 제대로 작업 시작 - 홈, 구독, 보관함 작업 후 시간이 남으면 채널 부분 도입.

---
+++ app/src/main/java/com/example/baetube/activity/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.baetube.activity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.baetube.HomeFragment;
+import com.example.baetube.R;
+import com.example.baetube.StorageFragment;
+import com.example.baetube.SubscribeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity
+{
+    // 바텀 네비게이션
+    private BottomNavigationView bottomNavigationView;
+    // 프래그먼트 매니저
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 툴바(액션바)를 설정
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // 프래그먼트 매니저를 지정
+        fragmentManager = getSupportFragmentManager();
+
+        // 프래그먼트 매니저에 HomeFragment를 추가하고 커밋한다. ( 첫 화면 지정 )
+        fragmentManager.beginTransaction().add(R.id.activity_main_frame, new HomeFragment()).commit();
+
+        // 바텀 네비게이션 요소를 findViewById를 사용하여 찾는다.
+        bottomNavigationView = findViewById(R.id.activity_main_bottom_navigation);
+
+        // 바텀 네비게이션 아이템 선택 이벤트.
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+            {
+
+                switch(item.getItemId())
+                {
+                    // 홈 아이콘 클릭 시 해당 프래그먼트로 리플레이스
+                    case R.id.menu_bottom_navigation_home :
+                        fragmentManager.beginTransaction().replace(R.id.activity_main_frame, new HomeFragment()).commit();
+                        break;
+                    // 업로드 아이콘 클릭 시.
+                    case R.id.menu_bottom_navigation_upload :
+                        // 업로드 액티비티로 전환해야 하므로 추후에 추가.
+                        break;
+                    // 구독 아이콘 클릭 시 해당 프래그먼트로 리플레이스
+                    case R.id.menu_bottom_navigation_subscribe :
+                        fragmentManager.beginTransaction().replace(R.id.activity_main_frame, new SubscribeFragment()).commit();
+                        break;
+                    // 보관함 아이콘 클릭 시 해당 프래그머늩로 리플레이스
+                    case R.id.menu_bottom_navigation_storage :
+                        fragmentManager.beginTransaction().replace(R.id.activity_main_frame, new StorageFragment()).commit();
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_toolbar, menu);
+
+        return true;
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        return false;
+    }
+
+}
